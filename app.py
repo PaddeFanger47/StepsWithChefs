b2d79d72699d3ac413517dd1674cc0ad92c783d2
PaddeFanger47 <martin2002.privat@gmail.com>
2025-06-02 15:04:46 +0200

    app

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def index():
     return "Hello, StepsWithChefs!"
 
+@app.route('/test')
+def test():
+    conn = sqlite3.connect('stepswithchefs.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    conn.close()
+    return f"Tabeller: {tables}"
+
 if __name__ == '__main__':
     app.run(debug=True)
