5737dda3ee7a391fb3305c852d80dcdf209a05fa
Yifannnnnnnnnn <yifan2sf@gmail.com>
2025-06-04 12:46:01 +0200

    templates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, StepsWithChefs!"
+    return render_template('index.html')
 
 @app.route('/test')
 def test():
