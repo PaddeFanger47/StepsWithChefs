6b6283588257b65d2c0ed91d7b4fe13f2576cbcd
PaddeFanger47 <martin2002.privat@gmail.com>
2025-06-04 14:06:03 +0200

    comments

--- sql/init.sql
+++ sql/init.sql
@@ -1,10 +1,11 @@
+-- From our E/R diagram we are creating the user function
 CREATE TABLE User (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT,
     profile_image TEXT,
     password TEXT
 ); 
-
+-- From our E/R diagram we are creating the recipe function
 CREATE TABLE Recipe (
     recipe_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
@@ -14,7 +15,7 @@ CREATE TABLE Recipe (
     media TEXT,
     FOREIGN KEY(user_id) REFERENCES User(user_id)
 );
-
+-- From our E/R diagram we are creating the comment function
 CREATE TABLE Comment (
   comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_id INTEGER,
@@ -25,14 +26,14 @@ CREATE TABLE Comment (
   FOREIGN KEY(user_id) REFERENCES User(user_id),
   FOREIGN KEY(recipe_id) REFERENCES Recipe(recipe_id)
 );
-
+-- From our E/R diagram we are creating the like function
 CREATE TABLE Like (
   user_id INTEGER,
   recipe_id INTEGER,
   FOREIGN KEY(user_id) REFERENCES User(user_id),
   FOREIGN KEY(recipe_id) REFERENCES Recipe(recipe_id)
 );
-
+-- From our E/R diagram we are creating the repost function
 CREATE TABLE Repost (
     repost_id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
@@ -42,31 +43,38 @@ CREATE TABLE Repost (
     FOREIGN KEY(recipe_id) REFERENCES Recipe(recipe_id)
 );
 
---Her tilføjer vi fiktive brugere:
+-- Users (We are adding fictive users with fictive pictures and names)
 INSERT INTO User (username, profile_image, password)
 VALUES ('AliceCHEF', 'img/AliceCHEF.jpg', '1234'),
        ('DonaldBen', 'img/DonaldBen.jpg', '4421'),
        ('KatrineMad', 'img/KatrineMad.jpg', '3312'),
        ('BOB444', 'img/BOB444.jpg', '5678');
-
+-- Recipe (Describing recipe with the decription, incredients and a picture)
 INSERT INTO Recipe (user_id, title, description, ingredients, media)
 VALUES 
 (1, 'Pancakes', 'Fluffy and light', 'flour, eggs, milk', 'pancakes.jpg'),
-(2, 'Spaghetti Bolognese', 'Italian classic', 'pasta, beef, tomato', 'spaghetti.jpg');
+(2, 'Spaghetti Bolognese', 'Italian classic', 'pasta, beef, tomato', 'spaghetti.jpg'),
+(4, 'Mashed Potatoes', 'Smooth and tasteful', 'potatoes, butter, milk', 'potatoes.jpg');
 
+-- Comments (adding comments to recipes with text, timestamp and rating for the food)
 INSERT INTO Comment (user_id, recipe_id, text, timestamp, rating)
 VALUES (1, 1, 'So good!', '2025-06-02 14:00', 5),
-       (2, 2, 'My kids loved it.', '2025-06-02 14:15', 4);
+       (2, 2, 'My kids loved it.', '2025-06-02 14:15', 4),
+       (1, 4, 'Soooooo smooth, incredible!', '2025-06-03 19:13', 5);
 
+-- Likes (adds the amount of likes to the recipe)
 INSERT INTO Like (user_id, recipe_id)
 VALUES 
     (1, 1), 
     (2, 2),
     (3, 1),
-    (4, 1);  -- Bob liker også opskrift 1
+    (4, 1),
+    (2, 4),
+    (3, 4);
 
--- Reposts (tilføj flere og med tidspunkt!)
+-- Reposts (adding repost with timestamp to post)
 INSERT INTO Repost (user_id, recipe_id, timestamp)
 VALUES 
-    (2, 1, '2024-06-02 15:45'),
-    (4, 2, '2024-06-02 18:00');
+    (2, 1, '2025-06-02 15:45'),
+    (4, 2, '2025-06-02 18:00'),
+    (1, 4, '2025-06-03 19:15');
