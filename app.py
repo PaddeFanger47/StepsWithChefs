ebb569406e398a4af6db5ecc08b947bddf0c9307
Yifannnnnnnnnn <yifan2sf@gmail.com>
2025-06-04 15:07:11 +0200

    slettet af test

--- app.py
+++ app.py
@@ -8,29 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    conn = sqlite3.connect('stepswithchefs.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    tables = cursor.fetchall()
-    conn.close()
-    return f"Tabeller: {tables}"
 
-@app.route('/users')
-def list_users():
-    conn = sqlite3.connect('stepswithchefs.db')
-    c = conn.cursor()
-    c.execute("SELECT user_id, username, profile_image FROM User")
-    users = c.fetchall()
-    conn.close()
-
-    # Returner som HTML (simpel liste)
-    output = "<h1>Users</h1><ul>"
-    for user in users:
-        output += f"<li>ID: {user[0]} | {user[1]} - <img src='/static/{user[2]}' alt='image' width='50'></li>"
-    output += "</ul>"
-    return output
 
 @app.route('/recipes')
 def list_recipes():
@@ -46,8 +24,6 @@ def list_recipes():
         output += f"<li><strong>{r[0]}</strong>: {r[1]}</li>"
     output += "</ul>"
     return output
-
-
 
 @app.route('/feed')
 def feed():
