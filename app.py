88919bd27a3787887cbf056ec252d5f51561426d
PaddeFanger47 <martin2002.privat@gmail.com>
2025-06-04 11:34:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,14 +20,29 @@ def test():
 def list_users():
     conn = sqlite3.connect('stepswithchefs.db')
     c = conn.cursor()
-    c.execute("SELECT username, profile_image FROM User")
+    c.execute("SELECT user_id, username, profile_image FROM User")
     users = c.fetchall()
     conn.close()
 
     # Returner som HTML (simpel liste)
     output = "<h1>Users</h1><ul>"
     for user in users:
-        output += f"<li>{user[0]} - <img src='{user[1]}' alt='image' width='50'></li>"
+        output += f"<li>ID: {user[0]} | {user[1]} - <img src='{user[2]}' alt='image' width='50'></li>"
+    output += "</ul>"
+    return output
+
+@app.route('/recipes')
+def list_recipes():
+    conn = sqlite3.connect('stepswithchefs.db')
+    c = conn.cursor()
+    c.execute("SELECT recipe_id, title, description, ingredients, media FROM Recipe")
+    recipes = c.fetchall()
+    conn.close()
+
+    # Returnerer som HTML 
+    output = "<h1>Recipes</h1><ul>"
+    for r in recipes:
+        output += f"<li><strong>{r[0]}</strong>: {r[1]}</li>"
     output += "</ul>"
     return output
 
