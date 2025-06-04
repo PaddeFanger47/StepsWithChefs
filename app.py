00093ba23c6e8ce5db14a09c7916cea226146fc2
PaddeFanger47 <martin2002.privat@gmail.com>
2025-06-04 11:19:27 +0200

    users

--- app.py
+++ app.py
@@ -16,5 +16,20 @@ def test():
     conn.close()
     return f"Tabeller: {tables}"
 
+@app.route('/users')
+def list_users():
+    conn = sqlite3.connect('stepswithchefs.db')
+    c = conn.cursor()
+    c.execute("SELECT username, profile_image FROM User")
+    users = c.fetchall()
+    conn.close()
+
+    # Returner som HTML (simpel liste)
+    output = "<h1>Users</h1><ul>"
+    for user in users:
+        output += f"<li>{user[0]} - <img src='{user[1]}' alt='image' width='50'></li>"
+    output += "</ul>"
+    return output
+
 if __name__ == '__main__':
     app.run(debug=True)
